095b02d7b7b16e7a5392a5dbbd7236c0b2e4eaa7
Leandro Amaro Garcia <>
2020-09-01 10:46:05 -0300

    correct question content width

--- components/Question/styles.js
+++ components/Question/styles.js
@@ -11,12 +11,14 @@ margin-bottom: 10px;
 :last-child {border:0;}
 
 .actions_content{display:flex;justify-content:center; align-items:center;}
+.question_content{min-width:500px;}
 .question_content .name_bold {font-weight:700;}
 .question_content .company_italic {font-style:italic;}
 
 
 @media (max-width: 860px) {
     display:flex; flex-direction:column; justify-content:space-between; align-items:center;
+    .question_content{min-width:100%;}
 }
 
 `;
